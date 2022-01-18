ae2f27369c7c8a21a5a7188b056280b5d4381ac8
Oleg Pedorenko <olezhkoo@gmail.com>
2022-01-19 01:29:00 +0200

    Parametrize success and error messages.

--- src/app/warning-alert/warning-alert.component.ts
+++ src/app/warning-alert/warning-alert.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core'
 
 @Component({ 
     selector: 'app-warning-alert',
-    template: `<h4>Warning! Something is wrong here....</h4>`,
+    template: `<h4>Warning! {{ message }}</h4>`,
     styles: [
         `h4 {
             color: orange
@@ -10,5 +10,5 @@ import { Component } from '@angular/core'
     ]
 })
 export class WarningAlertComponent { 
-
+    message: string = "Something is wrong here...."
 }
